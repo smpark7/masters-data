2b4f282a20a91afe6d878f1b8f2dfea9790c0fe2
Sun Myung Park <park_cg@live.com>
2019-10-08 13:51:29 -0500

    fix beta_eff and lambda data

--- serpent/fiss_plot.py
+++ serpent/fiss_plot.py
@@ -8,26 +8,38 @@ from pyne import nucname
 
 data = serpent.parse_det('msfr-full-core_det1b1.m')
 
-x_pow = data['DETfisxyX']
-y_pow = data['DETfisxyY']
-z_pow = data['DETfisxzZ']
-fissionx = data['DETfisxy']
-n = len(x_pow) * len(y_pow)
-m = len(x_pow) * len(z_pow)
-fissionx_den = np.reshape(fissionx.reshape((n,12))[:,10], \
-                         (len(x_pow), len(y_pow))) \
+r_pow = data['DETradialfissionR']
+phi_pow = data['DETradialfissionPHI']
+z_pow = data['DETradialfissionZ']
+fissionr = data['DETradialfission']
+n = len(r_pow) * len(phi_pow)
+m = len(r_pow) * len(z_pow)
+fissionr_den = np.reshape(fissionr.reshape((n,12))[:,10], \
+                         (len(r_pow), len(phi_pow))) \
                          #/ max(fission.reshape((57600,12))[:,10])
-fissionz = data['DETfisxz']
-fissionz_den = np.reshape(fissionz.reshape((m,12))[:,10], \
-                         (len(z_pow), len(x_pow))) \
+#fissionz = data['DETfisxz']
+#fissionz_den = np.reshape(fissionz.reshape((m,12))[:,10], \
+#                         (len(z_pow), len(x_pow))) \
+#
+#
+#fig, ax = plt.subplots(1,1,figsize=(12,10))
+#c = ax.pcolor(fissionx_den,cmap=cm.viridis)
+#fig.colorbar(c, ax=ax)
+#plt.savefig("powerxy.png", dpi=600)
+#
+#fig, ax = plt.subplots(1,1,figsize=(12,10))
+#c = ax.pcolor(fissionz_den,cmap=cm.viridis)
+#fig.colorbar(c, ax=ax)
+#plt.savefig("powerxz.png", dpi=600)
 
+plt.figure(figsize=(12,10))
+r = r_pow[:,2]
+area = np.zeros(len(r_pow))
+for i in range(len(r_pow)):
+    area[i] = (r_pow[i,1] ** 2 - r_pow[i,0] ** 2) * np.pi
+    fissionr_den[i] /= area[i]
+plt.plot(r, fissionr_den)
 
-fig, ax = plt.subplots(1,1,figsize=(12,10))
-c = ax.pcolor(fissionx_den,cmap=cm.viridis)
-fig.colorbar(c, ax=ax)
-plt.savefig("powerxy.png", dpi=600)
+data = serpent.parse_det('det-950k.m')
 
-fig, ax = plt.subplots(1,1,figsize=(12,10))
-c = ax.pcolor(fissionz_den,cmap=cm.viridis)
-fig.colorbar(c, ax=ax)
-plt.savefig("powerxz.png", dpi=600)
+#x_pow = data
